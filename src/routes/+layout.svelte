b513fc5b331a7445e2c6e2de7872f4d3cbaad3a3
okawibawa <okaa.wibawa@gmail.com>
2022-10-17 17:37:15 +0800

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+  import "@fontsource/mulish"
+</script>
+
+<style>
+  :global(body) {
+    font-family: "Mulish", sans-serif
+  }
+</style>
+
+<slot></slot>
---
+++ src/routes/[...slug]/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { page } from '$app/stores'
+  
+  const urlParams = $page.url.pathname
+  const removesUrlSlash = urlParams.replace('/', '')
+</script>
+
+<template>
+  {#if removesUrlSlash}
+    <h2>JWT: {removesUrlSlash}</h2>
+  {/if}
+  
+  {#if !removesUrlSlash}
+    <h2>No JWT provided.</h2>
+  {/if}
+</template>
